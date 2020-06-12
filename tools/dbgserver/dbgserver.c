67e3b04a79b389a49bff19bf827d36863cffc994
Andrei Formiga <archimedes_siracusa@hotmail.com>
2020-06-12 19:03:08 -0300

    add simple debug server

---
+++ tools/dbgserver/dbgserver.c
@@ -0,0 +1,85 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// 
+// A simple server that displays messages sent to it, meant
+// for debugging the Wii version
+//
+
+int main(int argc, char *argv[]) {
+	// port to start the server on
+	int SERVER_PORT = 8877;
+
+	// socket address used for the server
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+
+	// htons: host to network short: transforms a value in host byte
+	// ordering format to a short value in network byte ordering format
+	server_address.sin_port = htons(SERVER_PORT);
+
+	// htonl: host to network long: same as htons but to long
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// create a TCP socket, creation returns -1 on failure
+	int listen_sock;
+	if ((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf(stderr, "Could not create listen socket\n");
+		return 1;
+	}
+
+	// bind it to listen to the incoming connections on the created server
+	// address, will return -1 on error
+	if ((bind(listen_sock, (struct sockaddr *)&server_address,
+	          sizeof(server_address))) < 0) {
+		fprintf(stderr, "Could not bind socket\n");
+		return 1;
+	}
+
+	int wait_size = 16;  // maximum number of waiting clients, after which
+	                     // dropping begins
+	if (listen(listen_sock, wait_size) < 0) {
+		fprintf(stderr, "Could not open socket for listening\n");
+		return 1;
+	}
+
+	printf("=== Listening on port %d\n", SERVER_PORT);
+
+	// socket address used to store client address
+	struct sockaddr_in client_address;
+	int client_address_len = 0;
+
+	// main server loop
+	for (;;) {
+		// open a new socket to transmit data per connection
+		int sock;
+		if ((sock = accept(listen_sock, (struct sockaddr *)&client_address,
+		                   &client_address_len)) < 0) {
+			fprintf(stderr, "Could not open a socket to accept data\n");
+			return 1;
+		}
+
+		int n = 0;
+		int len = 0, maxlen = 1200;
+		char buffer[maxlen];
+
+		printf("=== Client connected with ip address: %s\n", inet_ntoa(client_address.sin_addr));
+
+		// keep running as long as the client keeps the connection open
+		while ((n = recv(sock, buffer, maxlen, 0)) > 0) {
+			buffer[n] = '\0';
+
+			printf("=> %s\n", buffer);
+		}
+
+		close(sock);
+	}
+
+	close(listen_sock);
+	return 0;
+}
---
+++ tools/dbgserver/sampleclient.c
@@ -0,0 +1,68 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+	const char* server_name = "localhost";
+	const int server_port = 8877;
+
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+
+	// creates binary representation of server name
+	// and stores it as sin_addr
+	// http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
+	inet_pton(AF_INET, server_name, &server_address.sin_addr);
+
+	// htons: port in network order format
+	server_address.sin_port = htons(server_port);
+
+	// open a stream socket
+	int sock;
+	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf(stderr, "could not create socket\n");
+		return 1;
+	}
+
+	// TCP is connection oriented, a reliable connection
+	// **must** be established before any data is exchanged
+	if (connect(sock, (struct sockaddr*)&server_address,
+	            sizeof(server_address)) < 0) {
+		fprintf(stderr, "could not connect to server\n");
+		return 1;
+	}
+
+	// send
+
+	// data that will be sent to the server
+	const char* data_to_send[] = 
+       { "Lorem ipsum dolor sit amet consectueris amaunensis foederis",
+         "ipsos literis quo vadis latensis est voegel vanitatis",
+         "al khwarizmi al jabr al alamiah al dente amaterasu humma kavullah",
+         "Be yourself; everyone else is already taken.",
+         "No man has a good enough memory to be a successful liar.",
+         "It was the best of times, it was the worst of times, it was the age of wisdom,",
+         "it was the age of foolishness, it was the epoch of belief, it was the epoch of", 
+         "incredulity, it was the season of Light, it was the season of Darkness, it was",
+         "the spring of hope, it was the winter of despair, we had everything before us,", 
+         "we had nothing before us, we were all going direct to heaven, we were all going", 
+         "direct the other way - in short, the period was so far like the present period,", 
+         "that some of its noisiest authorities insisted on its being received, for good", 
+         "or for evil, in the superlative degree of comparison only." };
+
+    printf("Connected, sending data...\n");
+
+    // send all the messages
+    for (int i = 0; i < 13; i++) {
+        printf("%d - %s\n", i, data_to_send[i]);
+	    send(sock, data_to_send[i], strlen(data_to_send[i]), 0);
+        sleep(5);
+    }
+
+	// close the socket
+	close(sock);
+	return 0;
+}
